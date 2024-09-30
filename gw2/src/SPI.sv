be744f448e16886778f51923d0acf64bdbdabdcf
Brian Hong <brian@hongs.me>
2024-09-30 07:59:23 -0400

    SPI Fixed

--- gw2/src/SPI.sv
+++ gw2/src/SPI.sv
@@ -21,7 +21,7 @@ endinterface
 
 module SPI_WB #(
     parameter CLK_FREQ = 200_000_000,
-    parameter SPI_FREQ = 10_000_000
+    parameter SPI_FREQ = 25_000_000
 ) (
     Wishbone_bus.S bus,
     SPI.M spi,
@@ -42,10 +42,11 @@ module SPI_WB #(
     always @(posedge bus.clk or posedge bus.rst) begin
         if (bus.rst) begin
             clk_counter <= 0;
+            spi.sck <= 1;
         end else begin
             if(spi_clk_en) begin
                 if(toggle) begin
-                    spi.sck = ~spi.sck;
+                    spi.sck <= ~spi.sck;
                     clk_counter <= 0;
                 end else begin
                     clk_counter <= clk_counter + 1;
@@ -76,6 +77,7 @@ module SPI_WB #(
             spi.cs_n <= 1;
             bus.ack <= 0;
             bus.stall <= 0;
+            spi_clk_en <= 0;
         end else begin
             if(bus_request) begin
                 spi.cs_n <= 0;
@@ -95,38 +97,41 @@ module SPI_WB #(
         end
     end
 
-    assign debug = {bus.cyc, bus.stb, bus.stall, bus.ack, spi_clk_en, 3'h0};
-
     // Transmit and shift on negedge
-    reg [31:0] txfifo = 32'h0;
+    reg [32:0] txfifo = 33'h0;
     reg [31:0] rxfifo = 32'h0;
     always @(posedge bus.clk or posedge bus.rst) begin
         if(bus.rst) begin
-            txfifo <= 32'h0;
+            txfifo <= 33'h0;
             rxfifo <= 32'h0;
         end else begin
             if(bus_request) begin
-                txfifo <= { 8'h03, bus.adr[23:0] };
+                txfifo <= { 1'h1, 8'h03, bus.adr[23:0] };
             end else if(clkn) begin
-                txfifo <= { txfifo[30:0], 1'b0 };
+                txfifo <= { txfifo[31:0], 1'b0 };
             end else if(clkp) begin
                 rxfifo <= { rxfifo[30:0], spi.miso };
             end
         end
     end
 
-    assign spi.mosi = txfifo[31];
+    assign spi.mosi = txfifo[32];
+
+    assign debug = { 8'h0 };
 
 endmodule
 
 
-module dummy_wb_master(
+module dummy_wb_master #(
+    parameter int OFFSET = 32'h100_000,
+) (
     Wishbone_bus.M bus,
     output [7:0] debug
 );
 
-    reg [31:0] counter = 32'h0;
-    assign bus.adr = counter;
+    reg [4:0] counter = 0;
+    assign bus.adr = OFFSET + counter;
+
     assign bus.dat_mosi = 32'hXXXXXXXX;
     assign bus.we = 0;
     
@@ -139,7 +144,7 @@ module dummy_wb_master(
             bus.cyc <= 0;
             bus.stb <= 0;
 
-            counter <= 32'h0;
+            counter <= 32'h50000;
         end else begin
             if(idle) begin
                 div <= div + 1;
@@ -151,7 +156,7 @@ module dummy_wb_master(
             end else if(bus.ack) begin
                 bus.cyc <= 0;
                 counter <= counter + 1;
-                // debug <= bus.dat_miso[7:0];
+                debug <= bus.dat_miso[7:0];
             end else if(bus.stb) begin
                 bus.stb <= 0;
             end
@@ -160,7 +165,7 @@ module dummy_wb_master(
 
     // assign debug = counter[7:0];
     // assign debug = {bus.cyc, bus.stall, bus.ack, bus.adr[4:0]};
-    assign debug = {bus.cyc, bus.dat_miso[6:0]};
+    // assign debug = {bus.cyc, bus.dat_miso[6:0]};
 
 endmodule
 
@@ -211,7 +216,7 @@ module spi_tb;
         // bus.dat_miso = 32'hAA;
         miso = 1;
 
-        #4000 $finish();
+        #10000 $finish();
     end
 
 endmodule
